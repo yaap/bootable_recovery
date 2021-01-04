d654688030a3cfcfa0eb504c8982f0d2513cd846
Ethan Yonker <dees_troy@teamw.in>
2021-01-03 23:47:09 -0800

    recovery: add support for changing slots
    * reference: https://github.com/omnirom/android_bootable_recovery/commit/1b190166eb1295c6339f6100e4fbb92c81b81ea6
    
    idoybh: Adapted to clean AOSP recovery
    
    Change-Id: I848e003ca569dfaa572ce7641775f0ea9f2b5513

--- recovery_ui/device.cpp
+++ recovery_ui/device.cpp
@@ -37,6 +37,7 @@ static std::vector<std::pair<std::string, Device::BuiltinAction>> g_menu_actions
   { "Wipe cache partition", Device::WIPE_CACHE },
   { "Mount /system", Device::MOUNT_SYSTEM },
   { "View recovery logs", Device::VIEW_RECOVERY_LOGS },
+  { "Switch slot", Device::SWAP_SLOT },
   { "Run graphics test", Device::RUN_GRAPHICS_TEST },
   { "Run locale test", Device::RUN_LOCALE_TEST },
   { "Enter rescue", Device::ENTER_RESCUE },
